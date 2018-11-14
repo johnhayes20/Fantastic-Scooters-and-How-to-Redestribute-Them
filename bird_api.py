fe8510bcd8dc8e3d31d353b8e9348787c2f5f875
johnhayes20 <john.hayes.20@gmail.com>
2018-11-13 16:27:02 -0800

    data cleaning and creating maps

--- bird_api.py
+++ bird_api.py
@@ -2,11 +2,12 @@ import requests
 import uuid
 import json
 import pandas as pd
+import numpy as np
 import datetime
 from time import sleep
 
 
-def access_bird_api_30():
+def access_bird_api():
     
     #Oakland center latitude
     lat = 37.808136
@@ -28,24 +29,28 @@ def access_bird_api_30():
     token = 'Bird ' + token_file.readlines()[0]
     
     # first time, write with header
-    
     g = requests.get(url= 'https://api.bird.co/bird/nearby?latitude='+str(lat_long[0][0])+'&longitude='+str(lat_long[0][1])+'&radius=1000',
                     headers= {'authorization': token, 'device-id':device_id, 'app-version':'4.7.3.1', 'location': '{"latitude":'+str(lat_long[0][0])+',"longitude":'+str(lat_long[0][1])+'}', 'radius':'1000'}
                     )
 
     #Add time column
     now = str(datetime.datetime.now())
+    time_group = str(datetime.datetime.now())
     data = g.json()
     for j in range(len(data['birds'])):
         data['birds'][j]['time'] = now
+        data['birds'][j]['time_group'] = time_group
+        data['birds'][j]['location_group'] = 0
 
     bird_df = pd.DataFrame.from_records(data['birds'])
-    with open('bird_data.csv', mode='a') as f:
+    with open('bird_data16.csv', mode='a') as f:
         f.write(bird_df.to_csv(header=True, index=False))
     
+    
+    # first time, write the next 29 without header 
     for i in range(len(lat_long)):
-            sleep(4)
-            if i == 1:
+            sleep(15)
+            if i == 0:
                 continue
             g = requests.get(url= 'https://api.bird.co/bird/nearby?latitude='+str(lat_long[i][0])+'&longitude='+str(lat_long[i][1])+'&radius=1000',
                             headers= {'authorization': token, 'device-id':device_id, 'app-version':'4.7.3.1', 'location': '{"latitude":'+str(lat_long[i][0])+',"longitude":'+str(lat_long[i][1])+'}', 'radius':'1000'}
@@ -56,16 +61,20 @@ def access_bird_api_30():
             data = g.json()
             for j in range(len(data['birds'])):
                 data['birds'][j]['time'] = now
+                data['birds'][j]['time_group'] = time_group
+                data['birds'][j]['location_group'] = i
 
             bird_df = pd.DataFrame.from_records(data['birds'])
-            with open('bird_data.csv', mode='a') as f:
+            with open('bird_data16.csv', mode='a') as f:
                 f.write(bird_df.to_csv(header=False, index=False))
+            print ('location {} of the grid was written according to {} time group'.format(i, time_group))
     
     
-    
+    # main function, write without header
     while True:
+        time_group = str(datetime.datetime.now())
         for i in range(len(lat_long)):
-            sleep(4)
+            sleep(15)
             g = requests.get(url= 'https://api.bird.co/bird/nearby?latitude='+str(lat_long[i][0])+'&longitude='+str(lat_long[i][1])+'&radius=1000',
                             headers= {'authorization': token, 'device-id':device_id, 'app-version':'4.7.3.1', 'location': '{"latitude":'+str(lat_long[i][0])+',"longitude":'+str(lat_long[i][1])+'}', 'radius':'1000'}
                             )
@@ -75,10 +84,12 @@ def access_bird_api_30():
             data = g.json()
             for j in range(len(data['birds'])):
                 data['birds'][j]['time'] = now
+                data['birds'][j]['time_group'] = time_group
+                data['birds'][j]['location_group'] = i
 
             bird_df = pd.DataFrame.from_records(data['birds'])
-            with open('bird_data.csv', mode='a') as f:
+            with open('bird_data16.csv', mode='a') as f:
                 f.write(bird_df.to_csv(header=False, index=False))
-        print ('all 30 locations have been written to the csv {}'.format(now))
+            print ('location {} of the grid was written according to {} time group'.format(i, time_group))
         
 access_bird_api()
